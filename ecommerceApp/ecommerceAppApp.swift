d23434e125d8f39591370cffcac29da3b32f0755
maazkhan2 <maazkhanswat02@gmail.com>
2024-06-25 11:50:17 +0500

    Initial Commit

---
+++ ecommerceApp/ecommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceAppApp.swift
+//  ecommerceApp
+//
+//  Created by Maaz Khan on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
